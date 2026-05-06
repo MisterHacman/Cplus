3bf0352ef507978d775a41f3196abc0d3eb3bf19
MisterHacman <138234335+MisterHacman@users.noreply.github.com>
2026-05-06 15:29:48 +0200

    Documented lexer.rb and added alterations to the interpreter

--- interpreter.rb
+++ interpreter.rb
@@ -2,7 +2,7 @@ require "io/console"
 
 class Interpreter
     def initialize(lexer, debug=false)
-        @variables = [0,0,0,0,0,0,0,0,0,0,0]
+        @variables =* 1..12
         @lexer = lexer
         while @lexer.next_token![0] != :eof
         end
@@ -12,10 +12,10 @@ class Interpreter
     end
 
     def print_vars
-        var_names = ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"]
+        var_names = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
         print "#{@current_inst_str}: \t["
         @variables.each_with_index do |var, index|
-            print "#{var_names[index]}: #{var}, " if var != 0
+            print "#{var_names[index]}: #{var}, "
         end
         puts "]"
     end
@@ -25,9 +25,9 @@ class Interpreter
         case @lexer.tokens[@index]
         in :eof, _
             return false
-        in :chord, [note, extension, base]
-            @current_inst_str = "#{note}#{extension}#{"/#{base}" if base}"
-            execute_chord!(note, extension, base)
+        in :chord, [note, quality, extension, alteration, base]
+            @current_inst_str = "#{note}#{quality}#{extension}#{alteration}#{"/#{base}" if base != ""}"
+            execute_chord!(note, quality, extension, alteration, base)
         in :base, note
             @current_inst_str = "/#{note}"
             char = STDIN.getch
@@ -53,73 +53,99 @@ class Interpreter
         return true
     end
 
-    def execute_chord!(note, extension, base)
+    def execute_chord!(note, quality, extension, alteration, base)
         var = var_index note
-        if !extension && base
-        elsif !extension && !base
-            if @debug
-                print "Print: "
-                p @variables[var_index note].chr
+        if quality == "" && extension == "" && alteration == "" && base == ""
+            print_var(var)
+            return nil
+        end
+        if quality == ""
+            execute_extension!(var, extension)
+        elsif quality[0] == "m" || quality[0] == "-"
+            execute_add!(note, extension, -1)
+        elsif quality[0, 3] == "aug" || quality[0] == "+"
+            execute_add!(note, extension, 1)
+        end
+        execute_alteration!(var, alteration)
+        if base != ""
+            @variables[var_index base] = @variables[var]
+        end
+    end
+
+    def print_var(var)
+        if @debug
+            print "Print: "
+            p @variables[var].chr
+        else
+            if @variables[var].chr == "\r"
+                print "\n"
             else
-                if @variables[var].chr == "\r"
-                    print "\n"
-                else
-                    print @variables[var_index note].chr
-                end
+                print @variables[var].chr
             end
-        elsif "-m".include?(extension[0]) || extension[0] == "+"
-            execute_add!(note, extension, "-m".include?(extension[0]) ? -1 : 1)
-        elsif extension[0, 3] == "aug"
-            execute_add!(note, extension[2..], 1)
-        elsif extension[0] == "M"
+        end
+    end
+
+    def execute_extension!(var, extension)
+        if extension[0, 3] == "maj"
             num = 7
-            num = extension[1..].match(/(\d+)/).captures[0].to_i if extension[1]
+            result = extension[1..].match(/(\d+)/)
+            num = result.captures[0].to_i if result
             @variables[var] = num if @variables[var] < num
-        elsif extension[0, 3] == "sus"
-            num = 4
-            num = extension[3].to_i if extension[3]
-            if @variables[var] == num
-                @index += 1
-            end
-        elsif extension[0, 3] == "add"
-            @variables[var] = (@variables[var].to_s + extension[3..]).to_i
+        elsif extension[0, 4] == "add9"
+            @variables[var] = "#{@variables[var]}9".to_i
         elsif extension[0, 3] == "6/9"
             @variables[var] = 69
-        elsif extension[0].match?(/\d/)
+        elsif extension[0] && extension[0].match?(/\d/)
             num = extension.match(/(\d+)/).captures[0].to_i
             @variables[var] = num
         end
-        if base
-            @variables[var_index base] = @variables[var]
+        if extension.match?(/sus/)
+            num = 4
+            num = 2 if extension.include?("2")
+            @index += 1 if @variables[var] == num
+        end
+    end
+
+    def execute_alteration!(var, alteration)
+        if alteration.match?(/b5/)
+            @variables[var] *= 5
+        elsif alteration.match?(/b9|#9/)
+            @variables[var] *= 9
+        elsif alteration.match?(/#11/)
+            @variables[var] *= 11
+        elsif alteration.match?(/b13/)
+            @variables[var] *= 13
         end
     end
 
     def execute_add!(note, extension, sign)
         var = var_index note
-        if extension[1] == nil
+        if extension == ""
             @variables[var] += sign
-        elsif extension[1].match?(/\d/)
-            @variables[var] += sign * extension[1..].match(/(\d+)/).captures[0].to_i
+        elsif extension[1, 3] == "6/9"
+            @variables[var] += sign * 69
+        elsif extension[0].match?(/\d/)
+            @variables[var] += sign * extension.match(/(\d+)/).captures[0].to_i
         else
             @variables[var] *= sign
-            execute_chord!(note, extension[1..])
+            execute_chord!(note, "", extension, "", "")
         end
     end
 
     def var_index(note)
         case note
-        when "A" then 0
-        when "A#", "Bb" then 1
-        when "B" then 2
-        when "C" then 3
-        when "C#", "Db" then 4
-        when "D" then 5
-        when "D#", "Eb" then 6
-        when "E" then 7
-        when "F" then 8
-        when "F#", "Gb" then 9
-        when "G" then 10
-        when "G#", "Ab" then 11
+        when "C" then 0
+        when "C#", "Db" then 1
+        when "D" then 2
+        when "D#", "Eb" then 3
+        when "E" then 4
+        when "F" then 5
+        when "F#", "Gb" then 6
+        when "G" then 7
+        when "G#", "Ab" then 8
+        when "A" then 9
+        when "A#", "Bb" then 10
+        when "B" then 11
         end
     end
 
--- lexer.rb
+++ lexer.rb
@@ -1,5 +1,21 @@
 class Lexer
     attr_reader :tokens
+    
+    # Initialize a lexer object
+    #
+    # @param filename [String] the file which will be tokenized
+    # 
+    # @examples
+    #   Lexer.new("examples/hello-world.c+") #=>
+    #     Lexer(
+    #       @tokens = [],
+    #       @filename = "examples/hello-world.c+",
+    #       @buffer = "Gb+#9 Gb Abadd9 Ab+ Ab/A Ab+ Ab Ab+7 Ab Ab A+11 A Db+#11 Dbm Db Gadd9 Gm Gm G A Ab/Bb Bb+7 Bbm Bb Ab Abm7 Abm Ab",
+    #       @index = 0,
+    #       @begin_reprise = -1)
+    #
+    # @author
+    # August Stokes
     def initialize(filename)
         @tokens = []
         @filename = filename
@@ -8,86 +24,205 @@ class Lexer
         @begin_reprise = -1
     end
 
+    # Advance lexer one token, append the token to @tokens and return it.
+    #
+    # The tokens shape depends on the token type.
+    # A chord has the shape [:chord, [<note>, <quality>, <extension>, <alteration>, <base>]], <note> being the only required item, the rest can be "".
+    #
+    # A base has the shape [:base, <note>] and an end reprise,
+    # the shape [:end_reprise, [<jump_ptr>, <reps>]]. End of file has the simple shape [:eof, nil]
+    # 
+    # @return [Array] returns a token in the form [keyword, data]
+    # @raise [RuntimeError] if we hit end of file without closing a reprise
+    #
+    # @examples
+    #   Lexer(@buffer = "Gb+#9", @index = 0).get_next_token! #=> [:chord, ["Gb", "+", "", "#9", ""]]
+    #   Lexer(@buffer = "Gb+#9", @index = 5).get_next_token! #=> [:eof, nil]
+    #   Lexer(@buffer = "Gb+#9", @index = 6).get_next_token! #=> [:eof, nil]
+    #   Lexer(@buffer = "/A ||: B :||x5", @index = 0).get_next_token! #=> [:base, "A"]
+    #   Lexer(@buffer = "/A ||: B :||x5", @index = 2).get_next_token! #=> [:chord, ["B", "", "", "", ""]]
+    #   Lexer(@buffer = "/A ||: B :||x5", @index = 8).get_next_token! #=> [:end_reprise, [1, 5]]
+    #   Lexer(@buffer = "||: ", 3).get_next_token! #=> RuntimeError
+    #
+    # @author
+    # August Stokes
     def next_token!
         @tokens << get_next_token!
         return @tokens[@tokens.length - 1]
     end
 
+    # Advances to the next token and returns it.
+    # Use `next_token!` instead of this one, as it doesn't fully advance the lexer.
+    #
+    # @return [Array] the token which is returned
+    # @raise [RuntimeError] if we hit end of file without closing a reprise
+    #
+    # @examples
+    #   Lexer(@buffer = "Gb+#9", @index = 0).get_next_token! #=> [:chord, ["Gb", "+", "", "#9", ""]]
+    #   Lexer(@buffer = "Gb+#9", @index = 5).get_next_token! #=> [:eof, nil]
+    #   Lexer(@buffer = "Gb+#9", @index = 6).get_next_token! #=> [:eof, nil]
+    #   Lexer(@buffer = "/A ||: B :||x5", @index = 0).get_next_token! #=> [:base, "A"]
+    #   Lexer(@buffer = "/A ||: B :||x5", @index = 2).get_next_token! #=> [:chord, ["B", "", "", "", ""]]
+    #   Lexer(@buffer = "/A ||: B :||x5", @index = 8).get_next_token! #=> [:end_reprise, [1, 5]]
+    #   Lexer(@buffer = "||: ", 3).get_next_token! #=> RuntimeError
+    #
+    # @author
+    # August Stokes
     def get_next_token!
         while true
             case ch
+            # Eof
             in nil
                 if @begin_reprise != -1
                     raise error("you need to end reprise, found end of file", @begin_reprise, 3)
                 end
                 return :eof, nil
+            # Chord
             in "A".."G"
                 return :chord, next_chord!
+            # Base
             in "/"
                 next_ch!
-                return :base, next_note!
+                return :base, next_regex!($NOTE_REGEX)
+            # Start reprise
             in "|" if @buffer[@index, 3] == "||:"
-                if @begin_reprise != -1
-                    raise error("you need to end reprise before creating a new one", @begin_reprise, 3, "new one here", @index, 3)
-                end
-                @begin_reprise = @tokens.length
-                advance! 3
-                return get_next_token!
+                return next_start_reprise!
+            # End reprise
             in ":" if @buffer[@index, 3] == ":||"
-                if @begin_reprise == -1
-                    raise error("no beginning reprise", @index, 3)
-                end
-                reprise_ptr = @begin_reprise
-                @begin_reprise = -1
-                if advance!(3) == "x"
-                    next_ch!
-                    return :end_reprise, [reprise_ptr, Float::INFINITY] if @buffer[@index, 3] == "inf"
-                    num_iters = next_regex!(/\G(\d+)/).to_i
-                    return :end_reprise, [reprise_ptr, num_iters]
-                end
-                return :end_reprise, [reprise_ptr, 1]
+                return next_end_reprise!
+            # Comment
             in "#"
                 while next_ch! != "\n"
                 end
                 next_ch!
                 next
+            # Whitespace / unused characters
             in _
                 next_ch!
                 next
             end
         end
+
     end
 
+    # Advance start reprise. Since the begin reprise is not a token,
+    # rather just an index that the end_reprise can't restart from,
+    # it will return the token after this one.
+    #
+    # @return [Array] the proceding token
+    # @raise [RuntimeError] if there was an unended begin reprise previously
+    #
+    # @examples
+    #   Lexer(@filename = "examples/add.c+", @index = 5).next_start_reprise! #=> [:chord, ["C", "+", "7", "", ""]]
+    #   Lexer(@buffer = "||: ||:", index = 3).next_start_reprise! #=> RuntimeError
+    #
+    # @author
+    # August Stokes
+    def next_start_reprise!
+        if @begin_reprise != -1
+            raise error("you need to end reprise before creating a new one", @begin_reprise, 3, "new one here", @index, 3)
+        end
+        @begin_reprise = @tokens.length
+        advance! 3
+        return get_next_token!
+    end
+
+    # Advance end reprise. This returns a token of the form [:end_reprise, [<jump_ptr>, <reps>]], where
+    # <jump_ptr> is the index in @tokens we are jumping to when returning and <reps> is the amount of
+    # times to return.
+    #
+    # @return [Array] the end reprise token
+    # @raise [RuntimeError] if there is no matching begin reprise
+    #
+    # @examples
+    #   Lexer(@filename = "examples/add.c+", @index = 13).next_end_reprise! #=> [:end_reprise, [2, 5]]
+    #   Lexer(@buffer = ":||", @index = 0).next_end_reprise! #=> RuntimeError
+    #
+    # @author
+    # August Stokes
+    def next_end_reprise!
+        if @begin_reprise == -1
+            raise error("no beginning reprise", @index, 3)
+        end
+        reprise_ptr = @begin_reprise
+        @begin_reprise = -1
+        if advance!(3) == "x"
+            next_ch!
+            return :end_reprise, [reprise_ptr, Float::INFINITY] if @buffer[@index, 3] == "inf"
+            num_iters = next_regex!(/\G(\d+)/).to_i
+            return :end_reprise, [reprise_ptr, num_iters]
+        end
+        return :end_reprise, [reprise_ptr, 1]
+    end
+
+    $NOTE_REGEX = /(A#|Bb|C#|Db|D#|Eb|F#|Gb|G#|Ab|[A-G])/
+    $QUALITY_REGEX = /^(-|m|\+|aug|)/
+    $EXTENSION_REGEX = /^(((maj)?(7|9|11|13)|add9|6\/9|6)?(sus(4|2|))?)/
+    $ALTERATION_REGEX = /^((b5)?(b9|#9)?(#11)?(b13)?)/
+    $INVALID_REGEX = /((-|m).*(sus|#9)|(\+|aug).*(b5|b13)|9.*9|9sus2|11.*11|11sus[^2]|13.*13|6.*13|sus2.*9|sus[^2].*#11|b5.*#11)/
+
+    # Advance chord. Returns a token of the shape [:chord, [<note>, <quality>, <extension>, <alterations>, <base>]].
+    #
+    # @return [Array] the token
+    # @raise [RuntimeError] if chord is invalid, given by the $INVALID_REGEX regular expression
+    #
+    # @examples
+    #   Lexer(@buffer = "C").next_chord! #=> [:chord, ["C", "", "", "", ""]]
+    #   Lexer(@buffer = "Db9").next_chord! #=> [:chord, ["Db", "", "9", "", ""]]
+    #   Lexer(@buffer = "Dmb9").next_chord! #=> [:chord, ["D", "m", "", "b9", ""]]
+    #   Lexer(@buffer = "Dmb9/C").next_chord! #=> [:chord, ["D", "m", "", "b9", "C"]]
+    #   Lexer(@buffer = "D9b9").next_chord! #=> RuntimeError
+    #
+    # @author
+    # August Stokes
     def next_chord!
-        note = self.next_note!
-        extension = next_extension!
+        start_index = @index
+        note = next_regex! $NOTE_REGEX
+        quality = next_regex! $QUALITY_REGEX
+        extension = next_regex! $EXTENSION_REGEX
+        alteration = next_regex! $ALTERATION_REGEX
+        raise error("invalid chord", start_index, @index - start_index) if (quality + extension + alteration).match?($INVALID_REGEX)
+        base = ""
         if ch == "/"
             next_ch!
-            base = next_note!
+            base = next_regex! $NOTE_REGEX
         end
-        return note, extension, base
+        return note, quality, extension, alteration, base
     end
 
-    $NOTE_REGEX = /\G(A#|Bb|C#|Db|D#|Eb|F#|Gb|G#|Ab|[A-G])/
-    def next_note!
-        next_regex! $NOTE_REGEX
-    end
-
-    $EXTENSION_REGEX = /\G(sus(2|4|)|6\/9|(-|m|\+|aug)?((M|7|M7|6)(b5)?|(M?9|add9)(b5)?|(M?11|add11)(b5)?|M?13(b5)?|b5)?)/
-    def next_extension!
-        next_regex! $EXTENSION_REGEX
-    end
-
+    # Takes a regex and advances the lexer by the match. Returns the matching string.
+    #
+    # @param [Regexp] the regex
+    # @return [String] the matching string
+    # @raise [RuntimeError] if @index >= @buffer.length
+    #
+    # @examples
+    #   Lexer(@buffer = "110.", @index = 1).next_regex!(/^(\d+)/) #=> "10"
+    #   Lexer(@buffer = "110.", @index = 3).next_regex!(/^(\d+)/) #=> ""
+    #   Lexer(@buffer = "110.", @index = 5).next_regex!(/^(\d+)/) #=> RuntimeError
+    #
+    # @author
+    # August Stokes
     def next_regex!(regex)
-        if result = @buffer.match(regex, @index)
+        if result = @buffer[@index..].match(regex)
             advance! result.captures[0].length
-            return nil if result.captures[0] == ""
             return result.captures[0]
         else
-            return nil
+            return ""
         end
     end
 
+    # Advances @buffer n characters and returns the character at that position.
+    #
+    # @param [Integer] how many characters to advance
+    # @return [String, nil] the character at the end of the advancement, nil if we hit end of file
+    #
+    # @examples
+    #   Lexer(@buffer = "110.", @index = 0).advance!(2) #=> "0"
+    #   Lexer(@buffer = "110.", @index = 0).advance!(4) #=> nil
+    #
+    # @author
+    # August Stokes
     def advance!(n)
         for _ in 1..n
             next_ch!
@@ -95,6 +230,16 @@ class Lexer
         return ch
     end
 
+    # Advance @buffer one character and return the character at that position.
+    #
+    # @return [String, nil] the character, nil if we hit end of file
+    #
+    # @examples
+    #   Lexer(@buffer = "110.", @index = 1).next_ch! #=> "0"
+    #   Lexer(@buffer = "110.", @index = 3).next_ch! #=> nil
+    #
+    # @author
+    # August Stokes
     def next_ch!
         @index += 1
         return @buffer[@index]
@@ -103,5 +248,4 @@ class Lexer
     def ch
         return @buffer[@index]
     end
-
 end
